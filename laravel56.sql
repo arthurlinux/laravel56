afc9f8db6eea88e10e6ac494aaf2386042e63ccb
arthurlinux <alexandrotello1@gmail.com>
2024-04-29 16:36:40 -0600

    add bases de datos

--- laravel56.sql
+++ laravel56.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:8889
--- Tiempo de generación: 23-03-2024 a las 23:51:28
+-- Tiempo de generación: 29-04-2024 a las 22:34:41
 -- Versión del servidor: 5.7.39
 -- Versión de PHP: 7.4.33
 
@@ -47,6 +47,34 @@ INSERT INTO `admins` (`id`, `name`, `email`, `password`, `remember_token`, `crea
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `empresas`
+--
+
+CREATE TABLE `empresas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ciudad` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `municipio` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted_at` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `empresas`
+--
+
+INSERT INTO `empresas` (`id`, `nombre`, `direccion`, `telefono`, `email`, `ciudad`, `municipio`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(9, 'Ea est suscipit volu', 'Quia voluptatem quo', 'Inventore esse dolo', 'hogif@mailinator.com', 'Voluptate quidem dol', 'In dignissimos ut du', NULL, '2024-04-28 01:25:44', '2024-04-28 01:26:35'),
+(11, 'Voluptas suscipit po', 'Architecto culpa pl', 'Quaerat iste ut aliq', 'zafaneg@mailinator.com', 'Nihil nulla magni ni', 'Esse ratione ut eni', NULL, '2024-04-28 01:28:45', '2024-04-28 01:28:45'),
+(12, 'Voluptas suscipit po', 'Architecto culpa pl', 'Quaerat iste ut aliq', 'zafaneg@mailinator.com', 'Nihil nulla magni ni', 'Esse ratione ut eni', NULL, '2024-04-28 01:51:46', '2024-04-28 01:51:46');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `migrations`
 --
 
@@ -66,7 +94,34 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2018_06_07_135646_crete_admin_table', 2),
 (4, '2018_06_08_001421_create_admins_table', 3),
 (5, '2024_03_08_191640_create_posts_table', 4),
-(6, '2024_03_23_210115_create_usuarios_table', 4);
+(6, '2024_03_23_210115_create_usuarios_table', 4),
+(7, '2024_04_18_214522_create_empresas_table', 5),
+(10, '2024_04_18_214904_create_modulos_table', 5),
+(12, '2024_04_18_214617_create_tikets_imgs_table', 7),
+(15, '2024_04_18_214543_create_tickets_table', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modulos`
+--
+
+CREATE TABLE `modulos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `modulo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted_at` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `modulos`
+--
+
+INSERT INTO `modulos` (`id`, `modulo`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(4, 'Aut iusto aut', NULL, '2024-04-28 02:14:07', '2024-04-28 02:14:25'),
+(5, 'Ex eligendi sint des', NULL, '2024-04-28 02:14:19', '2024-04-28 02:14:19'),
+(6, 'Sed anim magnam quo', NULL, '2024-04-30 03:39:01', '2024-04-30 03:39:01');
 
 -- --------------------------------------------------------
 
@@ -102,6 +157,50 @@ CREATE TABLE `posts` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tickets`
+--
+
+CREATE TABLE `tickets` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `modulo_id` bigint(20) DEFAULT NULL,
+  `admins_id` bigint(20) DEFAULT NULL,
+  `titulo` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `descripcion` text COLLATE utf8_unicode_ci NOT NULL,
+  `prioridad` enum('ALTA','MEDIA','BAJA') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'BAJA',
+  `comentarios` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ip_address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` enum('NUEVO','AESPERA','ENPROG','PENDIENTE','CANCELADO','CERRADO') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'NUEVO',
+  `deleted_at` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `user_id`, `modulo_id`, `admins_id`, `titulo`, `descripcion`, `prioridad`, `comentarios`, `ip_address`, `status`, `deleted_at`, `created_at`, `updated_at`) VALUES
+(12, 13, 4, NULL, 'In totam dolorum vol', 'Facilis ducimus ut', 'MEDIA', 'Ullamco sint id tot', NULL, 'NUEVO', NULL, '2024-04-30 03:36:27', '2024-04-30 04:29:03'),
+(13, 13, 4, NULL, 'Aperiam qui voluptat', 'Autem qui velit quia', 'ALTA', 'Nostrud esse ex non', NULL, 'NUEVO', NULL, '2024-04-30 03:36:45', '2024-04-30 04:29:09');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tikets_imgs`
+--
+
+CREATE TABLE `tikets_imgs` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `tiket_id` int(11) NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `users`
 --
 
@@ -120,7 +219,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, 'arturo', 'admin@admin.com', '$2y$10$io53oO3aIY38QZPT2/eXq.kLszEop22tFOTq7j9ODMzOlpcDZIcT.', 'DTyxszr4mmhajPwjHYjzSgSqlz8z8Q6fS4vQY8zu0H5Ci6fuJAfp9OUUYV7C', '2024-03-09 00:55:56', '2024-03-09 00:55:56');
+(3, 'arturo', 'admin@admin.com', '$2y$10$io53oO3aIY38QZPT2/eXq.kLszEop22tFOTq7j9ODMzOlpcDZIcT.', 'DTyxszr4mmhajPwjHYjzSgSqlz8z8Q6fS4vQY8zu0H5Ci6fuJAfp9OUUYV7C', '2024-03-09 00:55:56', '2024-03-09 00:55:56'),
+(4, 'arthurlinux', 'arthurlinux@marmacore.com', '$2y$10$PHZtrp0Q1GQRa94IPUzGxukEAZtibQ9IogYrUVG8xokJNkYK6Em0.', NULL, '2024-04-18 02:18:30', '2024-04-18 02:18:30');
 
 -- --------------------------------------------------------
 
@@ -144,7 +244,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido_paterno`, `apellido_materno`, `email`, `sexo`, `created_at`, `updated_at`) VALUES
-(1, 'Juan Francisco', 'Vega', 'Quintero', 'admin@admin.com', 'Masculino', NULL, NULL);
+(13, 'Dolorum nulla vel om', 'Ut Nam magnam eum oc', 'Facere voluptas amet', 'vykifecep@mailinator.com', 'Et excepturi aut qui', '2024-04-27 06:19:02', '2024-04-27 06:19:02'),
+(14, 'Repudiandae laborum', 'Quis in id non dolo', 'Soluta sapiente minu', 'bopog@mailinator.com', 'Repudiandae ut ut ex', '2024-04-27 06:19:08', '2024-04-27 06:19:08'),
+(15, 'Dolor veniam sunt', 'Eligendi tempor dese', 'Ut odio dolore moles', 'vuhyzaf@mailinator.com', 'Qui laboriosam sequ', '2024-04-27 22:31:51', '2024-04-27 22:31:51');
 
 --
 -- Índices para tablas volcadas
@@ -157,9 +259,21 @@ ALTER TABLE `admins`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `migrations`
 --
 ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `modulos`
+--
+ALTER TABLE `modulos`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -172,6 +286,18 @@ ALTER TABLE `password_resets`
 -- Indices de la tabla `posts`
 --
 ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tickets`
+--
+ALTER TABLE `tickets`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tikets_imgs`
+--
+ALTER TABLE `tikets_imgs`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -198,9 +324,21 @@ ALTER TABLE `admins`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de la tabla `empresas`
+--
+ALTER TABLE `empresas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de la tabla `modulos`
+--
+ALTER TABLE `modulos`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
@@ -210,16 +348,28 @@ ALTER TABLE `posts`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `tickets`
+--
+ALTER TABLE `tickets`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `tikets_imgs`
+--
+ALTER TABLE `tikets_imgs`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
